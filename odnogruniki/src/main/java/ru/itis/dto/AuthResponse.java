3ef3fc107987d3ab3fc3beaefe697dcc75dc659c
VeronikaYamancheva <vshrony@mail.ru>
2024-11-26 16:05:55 +0400

    task-03 initial commit from practice

---
+++ odnogruniki/src/main/java/ru/itis/dto/AuthResponse.java
@@ -0,0 +1,19 @@
+package ru.itis.dto;
+
+
+import lombok.*;
+
+@Data
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private int status;
+
+    private String statusDesc;
+
+    private UserDataResponse user;
+
+}
